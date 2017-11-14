c92ab9539d8efefcd8a0d70a97eb9792e1403072
Paulius Taparauskas <Paulius Taparauskas>
2017-11-14 19:36:11 +0200

    Add project files.

---
+++ 1 -1 uzduotis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1__1_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vardas = "paulius";
+            var pavarde = "taparauskas";
+            var amzius = 27;
+            var ugis = 1.85f; // cm
+            var svoris = 95; // kg
+            var aukstoji_mokykla = "kauno kolegija";
+            var akademines_grupes_kodas = 'b';
+            var kursas = 1;
+            var studiju_programos_pavadinimas = "programavimas";
+            var atsiskaitytu_kreditu_skaicius = 69;
+            Console.WriteLine 
+        }
+    }
+}
